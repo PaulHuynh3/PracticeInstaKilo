3f2160b789d60c3954346f65869377857a87d342
Paul <paul.huynh3@gmail.com>
2017-11-14 23:04:28 -0500

    initial

---
+++ PracticeInstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeInstaKilo
+//
+//  Created by Paul on 2017-11-14.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticeInstaKilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PracticeInstaKilo
+//
+//  Created by Paul on 2017-11-14.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
