4301f5e475d7dca11b54e88706246242d38e28ab
Paul <paul.huynh3@gmail.com>
2017-11-15 00:06:25 -0500

    create master vc

---
+++ PracticeInstaKilo/Controller/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  PracticeInstaKilo
+//
+//  Created by Paul on 2017-11-15.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UIViewController
+
+@end
